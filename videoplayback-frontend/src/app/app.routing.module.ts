847440ba86daf19769e6b32ad4f826f7bae62f07
Ashutosh <ashutosh.mahato@gmail.com>
2024-04-21 23:51:58 +0530

    Added path in app routing

--- videoplayback-frontend/src/app/app.routing.module.ts
+++ videoplayback-frontend/src/app/app.routing.module.ts
@@ -3,6 +3,6 @@ import { UploadVideoComponent } from "./upload-video/upload-video.component";
 const routes: Route = [
     {
         path: 'upload-video', component: UploadVideoComponent,
-        
+        path: 'save-video-details' component: SaveVideoDetailsComponent,
     }
-]
+];
