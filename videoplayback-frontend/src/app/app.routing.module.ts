623bfd2615cb716c3fec76d3f110c4cf95e23887
Ashutosh <ashutosh.mahato@gmail.com>
2024-04-22 23:42:04 +0530

    Added code for routing module

--- videoplayback-frontend/src/app/app.routing.module.ts
+++ videoplayback-frontend/src/app/app.routing.module.ts
@@ -6,7 +6,7 @@ const routes: Route = [
         path: 'upload-video', component: UploadVideoComponent,
     },
     {
-        path: 'save-video-details' component: SaveVideoDetailsComponent,
+        path: 'save-video-details/:videoId', component: SaveVideoDetailsComponent,
     }
 ];
 
