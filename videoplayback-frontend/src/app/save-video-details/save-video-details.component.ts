1fa92bb2acfca9ca7c6a7f09a8d2d3dd93973635
Ashutosh <ashutosh.mahato@gmail.com>
2024-05-04 23:39:36 +0530

    Added import for chipinputeditedevent

--- videoplayback-frontend/src/app/save-video-details/save-video-details.component.ts
+++ videoplayback-frontend/src/app/save-video-details/save-video-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { MatChipInputEvent } from "@angular/material/chips";
+import { MatChipEditedEvent } from "@angular/material/chips";
 
 @Component({
   selector: 'app-save-video-details',
