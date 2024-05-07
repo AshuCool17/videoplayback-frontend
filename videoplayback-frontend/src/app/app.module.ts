90ced79b49aea67371e7f92c77eff5a7d0267e09
Ashutosh <ashutosh.mahato@gmail.com>
2024-05-07 21:23:36 +0530

    Added vgcontrolsmodule

--- videoplayback-frontend/src/app/app.module.ts
+++ videoplayback-frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
-import {VgCoreModule} from '@videogular/ngx-videogular/core';
+import { VgCoreModule } from '@videogular/ngx-videogular/core';
+import { VgControlsModule } from '@videogular/ngx-videogular/controls';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
