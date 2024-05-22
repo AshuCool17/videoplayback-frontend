733a2851e37264a8a8573b497690ca8c8e9fdc3b
Ashutosh <ashutosh.mahato@gmail.com>
2024-05-22 23:37:57 +0530

    Added thumbnailurl in videodto

--- videoplayback-frontend/src/app/video-dto.ts
+++ videoplayback-frontend/src/app/video-dto.ts
@@ -1,8 +1,9 @@
-export interface VideoDto{
+export class VideoDto{
     id: string;
     title: string;
     descrption: string;
     tags: Array<string>;
     videoUrl: string;
     videoStatus: string;
+    thumbnailUrl: string;
 }
