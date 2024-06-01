7a6553d1c5ea482febe361c41b8cc91cacbf93ed
Ashutosh <ashutosh.mahato@gmail.com>
2024-06-01 20:47:22 +0530

    Added NgModule

--- videoplayback-frontend/src/app/auth/auth-config.module.ts
+++ videoplayback-frontend/src/app/auth/auth-config.module.ts
@@ -1,4 +1,11 @@
 import { NgModule } from '@angular/core';
 import { AuthModule } from 'angular-auth-oidc-client';
 
+@NgModule({
+    imports: AuthModule.forRoot({
+        config: {
+            authority: 'goprogramming.eu.auth.com',
+        }
+    })
+})
 export class AuthConfigModule{}
