d66e944254faa456ac632edf361875b48fe89341
Ashutosh <ashutosh.mahato@gmail.com>
2024-06-02 18:48:57 +0530

    Added redirecturl & clientid

--- videoplayback-frontend/src/app/auth/auth-config.module.ts
+++ videoplayback-frontend/src/app/auth/auth-config.module.ts
@@ -5,6 +5,9 @@ import { AuthModule } from 'angular-auth-oidc-client';
     imports: AuthModule.forRoot({
         config: {
             authority: 'goprogramming.eu.auth.com',
+            redirectUrl: window.location.origin,
+            clientId: 'client-id',
+            
         }
     })
 })
