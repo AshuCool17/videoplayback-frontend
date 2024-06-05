a92e2325b2b734d85ee44cd86463d6624d2cc551
Ashutosh <ashutosh.mahato@gmail.com>
2024-06-05 23:22:38 +0530

    Modified authority url

--- videoplayback-frontend/src/app/auth/auth-config.module.ts
+++ videoplayback-frontend/src/app/auth/auth-config.module.ts
@@ -4,7 +4,7 @@ import { AuthModule } from 'angular-auth-oidc-client';
 @NgModule({
     imports: [AuthModule.forRoot({
         config: {
-            authority: 'goprogramming.eu.auth.com',
+            authority: 'https://goprogramming.eu.auth.com',
             redirectUrl: window.location.origin,
             clientId: 'client-id',
             scope: 'openid profile offline_access',
