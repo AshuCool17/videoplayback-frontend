295e6487fd0edd97cb1ee12cb81e72066ec795c4
Ashutosh <ashutosh.mahato@gmail.com>
2024-06-13 19:33:04 +0530

    Added path for video detail

--- videoplayback-frontend/src/app/app.routing.module.ts
+++ videoplayback-frontend/src/app/app.routing.module.ts
@@ -7,6 +7,9 @@ const routes: Route = [
     },
     {
         path: 'save-video-details/:videoId', component: SaveVideoDetailsComponent,
+    },
+    {
+        path: 'video-details/:videoId', component: VideoDetailComponent,
     }
 ];
 
