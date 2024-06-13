78f7ff44963eb75915d00ed9c5debd2efe2d26e3
Ashutosh <ashutosh.mahato@gmail.com>
2024-06-13 19:30:33 +0530

    Added constructor

---
+++ videoplayback-frontend/src/app/video-detail/video-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-video-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './video-detail.component.html',
+  styleUrl: './video-detail.component.css'
+})
+export class VideoDetailComponent implements OnInit{
+
+  constructor() {}
+}
