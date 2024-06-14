2c48bd64d2207eecbe1a12002fe446c0b192b942
Ashutosh <ashutosh.mahato@gmail.com>
2024-06-14 23:59:02 +0530

    Added constructor

--- videoplayback-frontend/src/app/video-detail/video-detail.component.ts
+++ videoplayback-frontend/src/app/video-detail/video-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-video-detail',
@@ -9,7 +10,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VideoDetailComponent implements OnInit{
 
-  constructor() {}
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit: void(){
     
