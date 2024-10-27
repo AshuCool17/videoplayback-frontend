6cdb6fc2255b92a3a8f3b59c084697635dd4ea2e
Ashutosh <ashutosh.mahato@gmail.com>
2024-10-27 10:00:02 +0530

    Added oninit

--- videoplayback-frontend/src/app/video-card/video-card.component.ts
+++ videoplayback-frontend/src/app/video-card/video-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { VideoDto } from '../video-dto';
 
 @Component({
@@ -8,11 +8,13 @@ import { VideoDto } from '../video-dto';
   templateUrl: './video-card.component.html',
   styleUrl: './video-card.component.css'
 })
-export class VideoCardComponent {
+export class VideoCardComponent implements OnInit{
 
   @Input()
   video!: VideoDto;
 
   constructor(){}
+
+  ngOnInit(): void{}
 
 }
