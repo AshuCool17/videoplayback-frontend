ed71d9916295f941ee676a935acda2bb3721ce30
Ashutosh <ashutosh.mahato@gmail.com>
2024-11-12 15:32:17 +0530

    Added likeVideo method

--- videoplayback-frontend/src/app/video.service.ts
+++ videoplayback-frontend/src/app/video.service.ts
@@ -43,4 +43,8 @@ export class VideoService {
     getAllVideos(): Observable<Array<VideoDto>> {
         return this.httpClient.get<Array<VideoDto>>("http://localhost:8080/api/videos");
     }
+
+    likeVideo(){
+
+    }
 }
