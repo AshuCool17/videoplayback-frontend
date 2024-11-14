1d88c8e77c0393cc51acdf160a049ae9924b2228
Ashutosh <ashutosh.mahato@gmail.com>
2024-11-14 11:03:55 +0530

    Added observable return type in likeVideo method

--- videoplayback-frontend/src/app/video.service.ts
+++ videoplayback-frontend/src/app/video.service.ts
@@ -44,7 +44,7 @@ export class VideoService {
         return this.httpClient.get<Array<VideoDto>>("http://localhost:8080/api/videos");
     }
 
-    likeVideo(videoId: string){
+    likeVideo(videoId: string): Observable<VideoDto>{
         return this.httpClient.post<VideoDto>("http://localhost:8080/api/videos/" + videoId + "/like", null);
     }
 }
