38bc9ca81a0f12dba32d0087dd84606e512c5e40
Ashutosh <ashutosh.mahato@gmail.com>
2024-12-20 11:07:24 +0530

    Moved code to utility method

--- videoplayback-frontend/src/app/comments/comments.component.ts
+++ videoplayback-frontend/src/app/comments/comments.component.ts
@@ -46,6 +46,8 @@ export class CommentsComponent implements OnInit{
   }
 
   getComments(){
-    
+    this.commentService.getAllComments(this.videoId).subscribe(data=> {
+      this.commentsDto = data;
+      });
   }
 }
