a51276402c6686cd99cc09f4fa034156633e4f5d
Ashutosh <ashutosh.mahato@gmail.com>
2025-01-06 17:46:20 +0530

    Fixed import issue

--- videoplayback-frontend/src/app/app.module.ts
+++ videoplayback-frontend/src/app/app.module.ts
@@ -24,6 +24,7 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatSidenavModule } from '@angular/material/side-nav';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
